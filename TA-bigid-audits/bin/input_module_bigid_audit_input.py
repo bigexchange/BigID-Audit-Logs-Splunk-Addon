f13edb2418ce441f3059897ad3f9fa5103edc092
Juan Pablo Molina <jpmolina@bigid.com>
2025-01-09 15:58:30 -0300

    added check for null bytes

--- TA-bigid-audits/bin/input_module_bigid_audit_input.py
+++ TA-bigid-audits/bin/input_module_bigid_audit_input.py
@@ -22,7 +22,7 @@ def collect_events(helper, ew):
         r_al = get_audit_logs(helper, base_url, r_rt)
         helper.log_info('Audit logs successfully retrieved.')
         
-        audit_dumps = r_al.text.splitlines()
+        audit_dumps = r_al.splitlines()
         total_audit_dumps = len(audit_dumps)
         
         helper.log_info(f'Audit logs retrieved. A total of {str(total_audit_dumps)} lines. Now working on checkpoint matching...')
@@ -40,14 +40,17 @@ def collect_events(helper, ew):
                 index_to_start = index_to_start + 1
                 ad_line_hash = get_hexdigest_from_string(ad)
                 if checkpoint == ad_line_hash: 
-                    helper.log_info(f'Checkpoint found. Starting at line: {str(index_to_start)}.')
+                    helper.log_info(f'Checkpoint found. Starting at line: {str(index_to_start + 1)}.')
                     break
             helper.log_info(f'Checkpoint engine report: {str(index_to_start + 1)}/{total_audit_dumps}.')
         
         if index_to_start + 1 != total_audit_dumps:
             new_audit_logs = audit_dumps[index_to_start + 1:]
             for line in new_audit_logs:
-                event = helper.new_event(data=line, sourcetype="bigid:audit:logs", source=helper.get_input_stanza_names(), done=True, unbroken=True)
+                cleaned_line = remove_null_bytes(line)
+                if not cleaned_line:
+                    continue
+                event = helper.new_event(data=cleaned_line, sourcetype="bigid:audit:logs", source=helper.get_input_stanza_names(), done=True, unbroken=True)
                 ew.write_event(event)
             helper.log_info('New audit logs ingested successfully')
             
@@ -110,8 +113,8 @@ def get_audit_logs(helper, _base_url, _auth_token):
         if r.status_code != 200:
             helper.log_error(f'Unsuccessful HTTP request for BigID Audit Log endpoint. status_code={str(r.status_code)}')
             sys.exit(1)
-            
-        return r
+        
+        return r.text
     
     except Exception as e:
         helper.log_error(f'Error getting audit logs: {str(e)}')
@@ -120,3 +123,10 @@ def get_audit_logs(helper, _base_url, _auth_token):
 def get_hexdigest_from_string(line):
     line_hash = hashlib.sha256(line.strip().encode())
     return line_hash.hexdigest()
+
+def remove_null_bytes(data):
+    # If it's bytes, decode to string
+    if isinstance(data, bytes):
+        data = data.decode('utf-8', errors='ignore')
+    # Remove null bytes from string
+    return data.replace('\x00', '').replace('\\x00', '')
